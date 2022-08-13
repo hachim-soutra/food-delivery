1d3cfd8e41e88eb8583a4751a7426f0fa18326b4
hachim-soutra <hachimsoutra10@gmail.com>
2022-08-13 13:01:31 +0200

    create modal

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Cart from "./Components/Cart/Cart";
 import Header from "./Components/Layout/Header/Header";
 import Meals from "./Components/Meals/Meals";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header></Header>
       <main>
         <Meals />
